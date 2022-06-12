c4e8d29b6e771500fa46827b12e39a10d1be9901
valdimuhammad <valdimuhammad1908@gmail.com>
2022-06-12 13:15:57 +0700

    add-product_detail.dart

---
+++ lib/model/produk.dart
@@ -0,0 +1,18 @@
+class Produk{
+  int? id;
+  String? kodeProduk;
+  String? namaProduk;
+  int? hargaProduk;
+
+  Produk({this.id, this.kodeProduk, this.namaProduk, this.hargaProduk});
+
+  factory Produk.fromJson(Map<String, dynamic> obj) {
+    return Produk(
+      id: obj['kode_produk'],
+      kodeProduk: obj['kode_produk'],
+      namaProduk: obj['nama_produk'],
+      hargaProduk: obj['harga']
+    );
+  }
+
+}
